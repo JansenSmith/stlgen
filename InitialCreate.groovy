a0bd0b25abb622861d4890d35a83a394ca4d3500
JansemSmith <jansen.michael.smith@gmail.com>
2022-04-08 14:10:27 -0400

    Initial commit

--- InitialCreate.groovy
+++ InitialCreate.groovy
@@ -1,3 +1,6 @@
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.RoundedCube
+
 //Your code here
 
 CSG roundedCube = new RoundedCube(	40,// X dimention
@@ -6,4 +9,8 @@ CSG roundedCube = new RoundedCube(	40,// X dimention
 				)
 				.cornerRadius(4)// sets the radius of the corner
 				.toCSG()// converts it to a CSG tor display
+				
+				
+roundedCube.addExportFormat("stl")
+roundedCube.addExportFormat("svg")
 return roundedCube
