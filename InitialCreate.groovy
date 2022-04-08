7cf2b8e917ab226dc1b5857cdabfae93dd180cdf
JansemSmith <jansen.michael.smith@gmail.com>
2022-04-08 14:05:16 -0400

    Ignore the project files

--- InitialCreate.groovy
+++ InitialCreate.groovy
@@ -1,1 +1,9 @@
 //Your code here
+
+CSG roundedCube = new RoundedCube(	40,// X dimention
+				40,// Y dimention
+				40//  Z dimention
+				)
+				.cornerRadius(4)// sets the radius of the corner
+				.toCSG()// converts it to a CSG tor display
+return roundedCube
